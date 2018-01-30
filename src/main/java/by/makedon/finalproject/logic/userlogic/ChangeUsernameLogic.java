c5630e3cedbcdd1a5a60a32f609a7632e6fcfc0d
egor <egor.krutoychel.komp@mail.ru>
2018-01-30 17:35:16 +0300

    added validator

--- src/main/java/by/makedon/finalproject/logic/userlogic/ChangeUsernameLogic.java
+++ src/main/java/by/makedon/finalproject/logic/userlogic/ChangeUsernameLogic.java
@@ -4,9 +4,14 @@ import by.makedon.finalproject.dao.userdao.UserDAO;
 import by.makedon.finalproject.dao.userdao.UserDAOImpl;
 import by.makedon.finalproject.exception.DAOException;
 import by.makedon.finalproject.exception.LogicException;
+import by.makedon.finalproject.validator.UserValidator;
 
 public class ChangeUsernameLogic {
     public boolean doAction(String usernameValue, String newUsernameValue) throws LogicException {
+        if (!UserValidator.validateUsername(newUsernameValue)) {
+            throw new LogicException("wrong username");
+        }
+
         UserDAO dao = UserDAOImpl.getInstance();
         try {
             return dao.changeUsername(usernameValue, newUsernameValue);
