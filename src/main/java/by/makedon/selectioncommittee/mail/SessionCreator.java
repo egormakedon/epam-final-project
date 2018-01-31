96569d78bcdba870f18af5ad659d9ff823d4ea87
egor <egor.krutoychel.komp@mail.ru>
2018-01-31 12:03:20 +0300

    refactored mail session creator

--- src/main/java/by/makedon/selectioncommittee/mail/SessionCreator.java
+++ src/main/java/by/makedon/selectioncommittee/mail/SessionCreator.java
@@ -5,32 +5,49 @@ import javax.mail.Session;
 import java.util.Properties;
 
 public class SessionCreator {
-    private String smtpHost;
-    private String smtpPort;
-    private String userName;
+    private String username;
     private String userPassword;
     private Properties sessionProperties;
 
+    private static final String MAIL_SMTP_HOST = "mail.smtp.host";
+    private static final String MAIL_SMTP_PORT = "mail.smtp.port";
+    private static final String MAIL_USER_NAME = "mail.user.name";
+    private static final String MAIL_USER_PASSWORD = "mail.user.password";
+
+    private static final String MAIL_TRANSPORT_PROTOCOL = "mail.transport.protocol";
+    private static final String SMTP = "smtp";
+    private static final String MAIL_HOST = "mail.host";
+    private static final String MAIL_SMTP_AUTH = "mail.smtp.auth";
+    private static final String MAIL_SMTP_SOCKET_FACTORY_PORT = "mail.smtp.socketFactory.port";
+    private static final String MAIL_SMTP_SOCKET_FACTORY_CLASS = "mail.smtp.socketFactory.class";
+    private static final String MAIL_SMTP_SOCKET_FACTORY_FALLBACK = "mail.smtp.socketFactory.fallback";
+    private static final String MAIL_SMTP_QUITWAIT = "mail.smtp.socketFactory.fallback";
+
+    private static final String TRUE = "true";
+    private static final String FALSE = "false";
+    private static final String SSL_SOCKET_FACTORY_CLASS = "javax.net.ssl.SSLSocketFactory";
+
     SessionCreator(Properties configProperties) {
-        smtpHost = configProperties.getProperty("mail.smtp.host");
-        smtpPort = configProperties.getProperty("mail.smtp.port");
-        userName = configProperties.getProperty("mail.user.name");
-        userPassword = configProperties.getProperty("mail.user.password");
+        String smtpHost = configProperties.getProperty(MAIL_SMTP_HOST);
+        String smtpPort = configProperties.getProperty(MAIL_SMTP_PORT);
+        username = configProperties.getProperty(MAIL_USER_NAME);
+        userPassword = configProperties.getProperty(MAIL_USER_PASSWORD);
+
         sessionProperties = new Properties();
-        sessionProperties.setProperty("mail.transport.protocol", "smtp");
-        sessionProperties.setProperty("mail.host", smtpHost);
-        sessionProperties.put("mail.smtp.auth", "true");
-        sessionProperties.put("mail.smtp.port", smtpPort);
-        sessionProperties.put("mail.smtp.socketFactory.port", smtpPort);
-        sessionProperties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-        sessionProperties.put("mail.smtp.socketFactory.fallback", "false");
-        sessionProperties.setProperty("mail.smtp.quitwait", "false");
+        sessionProperties.setProperty(MAIL_TRANSPORT_PROTOCOL, SMTP);
+        sessionProperties.setProperty(MAIL_HOST, smtpHost);
+        sessionProperties.put(MAIL_SMTP_AUTH, TRUE);
+        sessionProperties.put(MAIL_SMTP_PORT, smtpPort);
+        sessionProperties.put(MAIL_SMTP_SOCKET_FACTORY_PORT, smtpPort);
+        sessionProperties.put(MAIL_SMTP_SOCKET_FACTORY_CLASS, SSL_SOCKET_FACTORY_CLASS);
+        sessionProperties.put(MAIL_SMTP_SOCKET_FACTORY_FALLBACK, FALSE);
+        sessionProperties.setProperty(MAIL_SMTP_QUITWAIT, FALSE);
     }
 
     public Session createSession() {
         return Session.getDefaultInstance(sessionProperties, new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(userName, userPassword);
+                return new PasswordAuthentication(username, userPassword);
             }
         });
     }
