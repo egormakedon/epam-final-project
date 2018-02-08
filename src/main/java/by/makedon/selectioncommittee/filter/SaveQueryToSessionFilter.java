496b32b63ee8ceec3945fa53ad1e51045e5d0212
egor <egor.krutoychel.komp@mail.ru>
2018-02-08 14:32:05 +0300

    add save query filter

---
+++ src/main/java/by/makedon/selectioncommittee/filter/SaveQueryToSessionFilter.java
@@ -0,0 +1,44 @@
+package by.makedon.selectioncommittee.filter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class SaveQueryToSessionFilter implements Filter{
+    private static final String LAST_PAGE = "lastPage";
+    private static final String COMMAND = "command";
+    private static final String CHANGE_LANG = "changelang";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+
+        String commandValue = req.getParameter(COMMAND);
+        if (commandValue != null && commandValue.equals(CHANGE_LANG)) {
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+
+        String lastPage = req.getServletPath() + "?" + req.getQueryString();
+        HttpSession session = req.getSession();
+        session.setAttribute(LAST_PAGE, lastPage);
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
