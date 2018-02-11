e8a992bb7e57b5e00f67ac9050d6c2bb7a582eef
egor <egor.krutoychel.komp@mail.ru>
2018-02-11 20:45:31 +0300

    added sending message to email to delete user command (notice)

--- src/main/java/by/makedon/selectioncommittee/logic/admin/DeleteUserLogic.java
+++ src/main/java/by/makedon/selectioncommittee/logic/admin/DeleteUserLogic.java
@@ -4,9 +4,15 @@ import by.makedon.selectioncommittee.dao.admin.AdminDAO;
 import by.makedon.selectioncommittee.dao.admin.AdminDAOImpl;
 import by.makedon.selectioncommittee.dao.base.BaseDAO;
 import by.makedon.selectioncommittee.dao.base.BaseDAOImpl;
+import by.makedon.selectioncommittee.dao.user.UserDAO;
+import by.makedon.selectioncommittee.dao.user.UserDAOImpl;
 import by.makedon.selectioncommittee.exception.DAOException;
 import by.makedon.selectioncommittee.exception.LogicException;
 import by.makedon.selectioncommittee.logic.Logic;
+import by.makedon.selectioncommittee.mail.MailBuilder;
+import by.makedon.selectioncommittee.mail.MailProperty;
+import by.makedon.selectioncommittee.mail.MailTemplatePath;
+import by.makedon.selectioncommittee.mail.MailThread;
 import by.makedon.selectioncommittee.validator.UserValidator;
 import com.sun.istack.internal.NotNull;
 
@@ -16,6 +22,7 @@ public class DeleteUserLogic implements Logic {
     private static final int LIST_SIZE = 1;
 
     private static final String ADMIN = "admin";
+    private static final String REMOVAL_NOTICE = "removal notice";
 
     @Override
     public void doAction(@NotNull List<String> parameters) throws LogicException {
@@ -31,6 +38,7 @@ public class DeleteUserLogic implements Logic {
 
         BaseDAO baseDAO = BaseDAOImpl.getInstance();
         AdminDAO adminDAO = AdminDAOImpl.getInstance();
+        UserDAO userDAO = UserDAOImpl.getInstance();
         try {
             if (!baseDAO.matchUsername(usernameValue)) {
                 throw new LogicException("user doesn't exist");
@@ -40,6 +48,15 @@ public class DeleteUserLogic implements Logic {
             if (type.equals(ADMIN)) {
                 throw new LogicException("you can't delete this user");
             }
+
+            String emailValue = userDAO.takeEmailByUsername(usernameValue);
+
+            String templatePath = MailTemplatePath.DELETE_USER.getTemplatePath();
+            MailBuilder mailBuilder = new MailBuilder(templatePath);
+            String mailText = String.format(mailBuilder.takeMailTemplate(), usernameValue);
+
+            MailThread mail = new MailThread(emailValue, REMOVAL_NOTICE, mailText, MailProperty.getInstance().getProperties());
+            mail.start();
 
             adminDAO.deleteEnrolleeFormByUsername(usernameValue);
             adminDAO.deleteUserByUsername(usernameValue);
--- src/main/java/by/makedon/selectioncommittee/mail/MailTemplatePath.java
+++ src/main/java/by/makedon/selectioncommittee/mail/MailTemplatePath.java
@@ -3,7 +3,8 @@ package by.makedon.selectioncommittee.mail;
 public enum MailTemplatePath {
     FORGOT_PASSWORD("/mailtemplates/forgotPassword.txt"),
     REGISTRATION("/mailtemplates/registration.txt"),
-    CHANGE_USER_DATA("/mailtemplates/changeUserData.txt");
+    CHANGE_USER_DATA("/mailtemplates/changeUserData.txt"),
+    DELETE_USER("/mailtemplates/deleteUser.txt");
 
     private String templatePath;
 
