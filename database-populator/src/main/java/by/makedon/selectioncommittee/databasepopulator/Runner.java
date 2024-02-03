3cb5a22f61538d7f7d1fa5a7c1413de3aed93051
Yahor Makedon <emakedonw@gmail.com>
2024-02-03 22:02:23 +0100

    Add Runner

---
+++ database-populator/src/main/java/by/makedon/selectioncommittee/databasepopulator/Runner.java
@@ -0,0 +1,38 @@
+package by.makedon.selectioncommittee.databasepopulator;
+
+import by.makedon.selectioncommittee.databasepopulator.util.ArgsUtil;
+import com.beust.jcommander.ParameterException;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Objects;
+
+/**
+ * @author Yahor Makedon
+ */
+@Slf4j
+public class Runner {
+    public static void main(String[] args) {
+        ArgsUtil argsUtil = null;
+        try {
+            argsUtil = new ArgsUtil(args);
+            run(argsUtil);
+        } catch (ParameterException e) {
+            log.error(e.getMessage(), e);
+            if (Objects.nonNull(argsUtil)) {
+                argsUtil.callHelp();
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    private static void run(ArgsUtil argsUtil) {
+        if (argsUtil.isHelp()) {
+            argsUtil.callHelp();
+        }
+        populateDatabase(argsUtil.getNumberOfEnrollees());
+    }
+
+    private static void populateDatabase(int numberOfEnrollees) {
+    }
+}
