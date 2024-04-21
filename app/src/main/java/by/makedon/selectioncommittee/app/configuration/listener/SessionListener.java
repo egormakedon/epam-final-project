519607564acb43cbc3441098d89b4da7ae6b5513
Yahor Makedon <emakedonw@gmail.com>
2024-04-21 14:31:35 +0200

    commit listener

--- app/src/main/java/by/makedon/selectioncommittee/app/configuration/listener/SessionListener.java
+++ app/src/main/java/by/makedon/selectioncommittee/app/configuration/listener/SessionListener.java
@@ -1,6 +1,9 @@
 package by.makedon.selectioncommittee.app.configuration.listener;
 
 import by.makedon.selectioncommittee.app.configuration.util.Page;
+import by.makedon.selectioncommittee.app.configuration.util.RequestParameterKey;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
@@ -8,27 +11,28 @@ import javax.servlet.http.HttpSessionListener;
 import java.util.Enumeration;
 
 public class SessionListener implements HttpSessionListener {
-    private static final String LANG = "lang";
-    private static final String LOGIN = "login";
+    private static final Logger logger = LoggerFactory.getLogger(SessionListener.class);
     private static final String EN = "en";
-    private static final String FALSE = "false";
-    private static final String LAST_PAGE = "lastPage";
 
     @Override
-    public void sessionCreated(HttpSessionEvent se) {
-        HttpSession session = se.getSession();
-        session.setAttribute(LANG, EN);
-        session.setAttribute(LOGIN, FALSE);
-        session.setAttribute(LAST_PAGE, Page.WELCOME);
+    public void sessionCreated(HttpSessionEvent event) {
+        HttpSession session = event.getSession();
+        session.setAttribute(RequestParameterKey.LANG, EN);
+        session.setAttribute(RequestParameterKey.LOGIN, Boolean.FALSE.toString());
+        session.setAttribute(RequestParameterKey.LAST_PAGE, Page.WELCOME);
+
+        logger.info("Session is created: {}", session.getId());
     }
 
     @Override
-    public void sessionDestroyed(HttpSessionEvent se) {
-        HttpSession session = se.getSession();
+    public void sessionDestroyed(HttpSessionEvent event) {
+        HttpSession session = event.getSession();
         Enumeration<String> attributeNames = session.getAttributeNames();
         while (attributeNames.hasMoreElements()) {
             String attribute = attributeNames.nextElement();
             session.removeAttribute(attribute);
         }
+
+        logger.info("Session is destroyed: {}", session.getId());
     }
 }
